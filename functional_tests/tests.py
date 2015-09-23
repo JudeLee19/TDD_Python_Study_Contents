5bd06d1dee773ffbd0cdbfbd4524105c1aeb1505
JudeLee <dongyub63@gmail.com>
2015-09-23 17:20:06 +0900

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
-
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/Users/judelee/Downloads/chromedriver')
         self.browser.implicitly_wait(3)
@@ -33,11 +33,6 @@ class NewVisitorTest(LiveServerTestCase):
         # centered there too
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
